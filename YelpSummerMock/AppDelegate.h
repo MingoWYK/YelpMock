7c7b39132cd2a465b873f590b1c2d542adf22272
Yuakai Weng <wengyuankai@Mingos-MacBook-Pro.local>
2017-08-26 17:39:30 -0400

    Initial Commit

---
+++ YelpSummerMock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YelpSummerMock
+//
+//  Created by wengyuankai on 8/26/17.
+//  Copyright © 2017 wengyuankai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YelpSummerMock/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YelpSummerMock
+//
+//  Created by wengyuankai on 8/26/17.
+//  Copyright © 2017 wengyuankai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
